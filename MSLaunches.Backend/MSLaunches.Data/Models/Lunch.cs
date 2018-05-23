f6cbd9a4b8259e3815c2b3c5d84e71648791098a
Luis Miguel Vittori <luismiguelvittori@gmail.com>
2018-05-23 11:29:38 -0300

    Missing model classes

---
+++ MSLaunches.Backend/MSLaunches.Data/Models/Lunch.cs
@@ -0,0 +1,32 @@
+﻿namespace MSLunches.Data.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+    /// <summary>
+    /// Represents the lunches available by day in our system
+    /// </summary>
+    public class Lunch : BaseEntity
+    {
+        public Lunch()
+        {
+            UserLunches = new HashSet<UserLunch>();
+        }
+
+        /// <summary>
+        /// Meal Id.
+        /// </summary>
+        [Required]
+        public Guid MealId { get; set; }
+
+        /// <summary>
+        /// Day of the meal
+        /// </summary>
+        [Required]
+        public DateTime Date { get; set; }
+
+        public virtual Meal Meal { get; set; }
+        public virtual ICollection<UserLunch> UserLunches { get; set; }
+    }
+}
---
+++ MSLaunches.Backend/MSLaunches.Data/Models/Meal.cs
@@ -0,0 +1,26 @@
+﻿namespace MSLunches.Data.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    /// <summary>
+    /// Represents the meal in our system
+    /// </summary>
+    public class Meal : BaseEntity
+    {
+        /// <summary>
+        /// Name of the meal.
+        /// </summary>
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Id of the meal type
+        /// </summary>
+        [Required]
+        public int TypeId { get; set; }
+
+        public virtual MealType MealType { get; set; }
+
+    }
+}
